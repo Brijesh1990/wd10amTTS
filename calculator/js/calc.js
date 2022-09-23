a150e67d28431fea4c8b2728d187c98d6531cdda
Brijesh1990 <bkpandey.pandey@gmail.com>
2022-09-23 11:28:47 +0530

    Add files via upload

--- calculator/js/calc.js
+++ calculator/js/calc.js
@@ -10,12 +10,9 @@ function displ1(val1)
 {
     document.getElementById("result").value+=val1;
 }
-
-
 function finalresult()
 {
-    // a=10;
-    // b=20;
+  
    let a=document.getElementById("result").value;
    let b=eval(a);
    document.getElementById("result").value=b;
